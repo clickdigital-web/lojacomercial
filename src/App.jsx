ffe8eda81e31b4a4c7c696501e069b3a4f3fda97
clickdigitalmkt <sitesclick67@gmail.com>
2026-08-12 11:09:56 -0400

    refactor: mantém apenas as duas logos no painel

--- src/App.jsx
+++ src/App.jsx
@@ -50,12 +50,6 @@ export default function App() {
       path: '/wp-content/uploads/2025/09/logo-click-preta.png',
       bgTheme: 'light',
     },
-    {
-      title: 'Favicon Click Digital',
-      filename: 'favicon-click-digital.png',
-      path: '/favicon-click-digital.png',
-      bgTheme: 'dark',
-    },
   ]
 
   if (!unlocked) {
